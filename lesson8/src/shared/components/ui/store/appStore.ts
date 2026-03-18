a85cd83db3b2eec37761f20c788141f616b5768d
manarkisan <manarkisan@gmail.com>
2026-03-18 16:02:58 +0000

    trying to do homework, making new code in appStore.tsx, starting again from scratch

--- lesson8/src/shared/components/ui/store/appStore.ts
+++ lesson8/src/shared/components/ui/store/appStore.ts
@@ -1,15 +1,56 @@
-import { create} from 'zustand'
+// import { ProductCard, type CartItem, type Product } from "@/features";
+// import { create, createStore } from "zustand";
+// import { useStore } from 'zustand'
+// import { subscribeWithSelector } from "zustand/middleware";
+// import { devtools } from "zustand/middleware";
+// import { DEFAULT_PRODUCTS as products } from '@/features/products/contants';
 
-interface AppStore {
-    count: number;
-    increment: () => void
-    decrement: () => void
-    reset: () => void
-}
+// export interface AppStore {
+//   items: CartItem[];
+//   addToCart: (productId: string, quantity: number) => void;
+//   removeFromCart: (productId: string) => void;
+  
+//   theme: 
+// }
 
-export const useAppStore = create<AppStore>((set) => ({
-    count: Number,
-    items: [],
-    increment: () => set((state) => ({}))
-    decrement: () => set((state) => ({}))
-}));
+// export const useAppStore = createStore<AppStore>() {
+//   devtools(
+//     subscribeWithSelector((set) => ({
+//         items: [],
+//         addToCart: (productId: string, quantity = 1) =>
+//             set((state) => {
+//                 const existing = state.items.find((i) => i.product.id === productId);
+//                 if (existing) {
+//                     return{
+//                         items: state.items.map((i) =>
+//                         i.product.is === productId
+//                     ? { ...i, quantity:i.quantity + quantity}
+//                 : i,
+//             )
+//                     }
+//                 }
+//                 const product = products.find((p) => p.id === productId);
+//                 if (product) {
+//                     return{
+//                         items: [...state.items, [ product, quantity]],
+//                     };
+//                 }
+//                 return state;
+//             }),
+//             removeFromCart: (productId: string) =>
+//                 set((state) => ({
+//                     items: state.items.filter(
+//                         (item: CartItem) => item.product.id !=== productId,
+//                     ),
+//                 })),
+//     }))
+//   )
+// };
+
+// function useAppStore(): AppStore;
+// function useAppStore<T>(selector: (state: AppStore) =>T): T;
+// function useAppStore<T>(selector? (state: AppStore) => T): AppStore | T {
+//     return useStore(useAppStore, (selector ?? ((s) => s)) as (state: AppStore) => T);
+// }
+
+// export { AppStore, useAppStore };
