7f2ef8171ac6e020b3c44cfc13b77b27143aae98
manarkisan <manarkisan@gmail.com>
2026-03-24 19:31:26 +0000

    trying to fix error...

--- lesson8/src/App.tsx
+++ lesson8/src/App.tsx
@@ -1,21 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Layout } from '@/components/Layout';
-import { AppStoreState } from '@/features/cart/context/CartContext';
+import { CartProvider } from '@/features/cart/context/CartContext';
 import { ProductsPage } from '@/pages/ProductsPage';
 import { CartPage } from '@/pages/CartPage';
 
 function App() {
   return (
-    <AppStoreState>
+    <CartProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<ProductsPage />} />
           <Route path="cart" element={<CartPage />} />
         </Route>
       </Routes>
-    </AppStoreState>
-    
+    </CartProvider>
   );
 }
 
