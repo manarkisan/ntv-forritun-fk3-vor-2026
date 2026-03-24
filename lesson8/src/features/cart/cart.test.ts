dc2681b12447243b121d0b416430d4f3a1697a9c
manarkisan <manarkisan@gmail.com>
2026-03-24 19:44:35 +0000

    test0

--- lesson8/src/features/cart/cart.test.ts
+++ lesson8/src/features/cart/cart.test.ts
@@ -1,3 +1,18 @@
+import type { Product } from '@/features/products/types'
+import { useCart } from './context/useCart';
+
 test('1 + 1 equals 2', () => {
   expect(1 + 1).toBe(2);
 });
+
+const mockProduct: Product = { id: "1", name: "test", price: 10 };
+
+test('Add Product to set wuantity', () => {
+  const { result } = renderHook(() => useCart(),{
+    wrapper: CartProvider,
+  });
+})
+
+function renderHook(arg0: () => any, p0: { wrapper: any; }): { result: any; } {
+  throw new Error('Function not implemented.');
+}
