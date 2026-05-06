8603bbf77e9d6cb33ed807844d8c2930159c9cbb
manarkisan <manarkisan@gmail.com>
2026-05-06 14:34:58 +0000

    cypress - 'Logging in / Fully test the login flow -- but only once!'

--- lesson14/cypress/e2e/home_page.cy.ts
+++ lesson14/cypress/e2e/home_page.cy.ts
@@ -1,16 +1,33 @@
-describe('The hoem page', () => {
+describe('The Login Page', () => {
   beforeEach(() => {
+    // reset and seed the database prior to every test
     cy.exec('npm run db:reset && npm run db:seed')
-    cy.request('POST', '/test/seed/post', {
-      title: 'First Post',
-      authorId: 1,
-      body: '...',
-    })
-        cy.request('POST', '/test/seed/user', { name: 'Jane' })
+
+    // seed a user in the DB that we can control from our tests
+    // assuming it generates a random password for us
+    cy.request('POST', '/test/seed/user', { username: 'jane.lane' })
       .its('body')
       .as('currentUser')
   })
-  it('succesfully loads', () => {
-    cy.visit('/')
+
+  it('sets auth cookie when logging in via form submission', function () {
+    // destructuring assignment of the this.currentUser object
+    const { username, password } = this.currentUser
+
+    cy.visit('/login')
+
+    cy.get('input[name=username]').type(username)
+
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`${password}{enter}`)
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/dashboard')
+
+    // our auth cookie should be present
+    cy.getCookie('your-session-cookie').should('exist')
+
+    // UI should reflect this user being logged in
+    cy.get('h1').should('contain', 'jane.lane')
   })
 })
