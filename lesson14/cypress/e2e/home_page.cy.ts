a3a80708637fb1fffbed95ded8cf48e7a5800dc0
manarkisan <manarkisan@gmail.com>
2026-05-06 14:32:36 +0000

    cypress - 'testing your app' - 'seeding  data'

--- lesson14/cypress/e2e/home_page.cy.ts
+++ lesson14/cypress/e2e/home_page.cy.ts
@@ -1,4 +1,15 @@
 describe('The hoem page', () => {
+  beforeEach(() => {
+    cy.exec('npm run db:reset && npm run db:seed')
+    cy.request('POST', '/test/seed/post', {
+      title: 'First Post',
+      authorId: 1,
+      body: '...',
+    })
+        cy.request('POST', '/test/seed/user', { name: 'Jane' })
+      .its('body')
+      .as('currentUser')
+  })
   it('succesfully loads', () => {
     cy.visit('/')
   })
